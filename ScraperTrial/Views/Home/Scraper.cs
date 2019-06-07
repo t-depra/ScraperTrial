f7872b2f5bd01dbbf7ce47227a7751ecd857b47d
Devanshi Pratap <t-depra@microsoft.com>
2019-06-07 16:42:57 -0700

    initial commit for scraper

---
+++ ScraperTrial/Views/Home/Scraper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using HtmlAgilityPack;
+using System.Xml;
+using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.InteropServices;
+using System.Net;
+using System.Text;
+
+[System.Serializable]
+public class Scraper
+{
+    public string Scraping()
+    {
+        string BingMapsKey = "AvCBj_jgkzN6BB4bx-dGT-vQkOoLtvPrApuuZzIem1pGoAyfbbwgL_JPGZsNNY28";
+        string url = "http://dev.virtualearth.net/REST/V1/Routes/Driving?o=xml&wp.0=london&wp.1=leeds&avoid=minimizeTolls&key=" + BingMapsKey;
+        HttpWebRequest HttpWReq = (HttpWebRequest)WebRequest.Create(url);
+        HttpWebResponse HttpWResp = (HttpWebResponse)HttpWReq.GetResponse();
+
+        var encoding = ASCIIEncoding.ASCII;
+        string responseText;
+        using (var reader = new System.IO.StreamReader(HttpWResp.GetResponseStream(), encoding))
+        {
+            responseText = reader.ReadToEnd();
+        }
+        HttpWResp.Close();
+        System.IO.File.WriteAllText(@"C:\Users\t-depra\Desktop\scraped.txt", responseText);
+        return responseText;
+    }
+}
+/*public class Scraper
+{
+    
+}*/
---
+++ ScraperTrial/Views/Home/ScraperStore.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+/*
+namespace ScraperTrial.Services
+{
+    public class ScraperFileStore
+    {
+        CloudBlobClient blobClient;
+        string baseUri = "https://scrapertrialstorage.blob.core.windows.net/";
+        public ScraperFileStore()
+        {
+            var credentials = new StorageCredentials("scrapertrialstorage", "V8lAf4DWsjSt2qJ4JwfyCaw7TKkTM2uKrtQ21B5y8Q7VqLxKUpGe8/9VpnqNFgiLndz1fa5DLsgd/ODfEg0ZHg==");
+            blobClient = new CloudBlobClient(new Uri(baseUri), credentials);
+        }
+
+        public Task<string> SaveScrapedFile(Stream fileStream)
+        {
+            var container = blobClient.GetContainerReference("scrapedfiles");
+            var blob = container.GetBlockBlobReference()
+        }
+    }
+}
+
+*/
+// Retrieve storage account from connection string.
+
+public class ScraperFileStore
+{
+    public void SaveScrapedFile()
+    {
+        string storageKey = "DefaultEndpointsProtocol=https;AccountName=scrapertrialstorage;AccountKey=lrP3oq1JRMU7sYsCDs4CHzL83VqPExKL4YW46aVcQzByG/2z2eNKFLFa2Eocge67/CO/zEklNKXnKKoWU/gQPw==;EndpointSuffix=core.windows.net";
+        CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageKey);
+        CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+        CloudBlobContainer container = blobClient.GetContainerReference("scrapedfiles");
+        CloudBlockBlob blockBlob = container.GetBlockBlobReference("scrapertrialstorage");
+        using (var fileStream = System.IO.File.OpenRead(@"C:\Users\t-depra\Desktop\scraped.txt"))
+        {
+            blockBlob.UploadFromStreamAsync(fileStream);
+        }
+    }
+}
+
+        
